ded9a51c32b03e3c0e1aa72445df0536fb1e803d
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2023-11-13 13:37:27 +0100

    feat: add /users/{id} endpoint

--- src/main/java/ch/fhnw/webec/exercise/controller/UserController.java
+++ src/main/java/ch/fhnw/webec/exercise/controller/UserController.java
@@ -1,16 +1,32 @@
 package ch.fhnw.webec.exercise.controller;
 
+import ch.fhnw.webec.exercise.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(path = "/user", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("title", "User");
 
         return "user";
+    }
+
+    @RequestMapping(path = "/users/{id}", method = RequestMethod.GET)
+    public String showBook(@PathVariable int id, Model model) {
+        model.addAttribute("book", this.userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+
+        return "user/show";
     }
 }

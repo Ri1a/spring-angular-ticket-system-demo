77662375b880dbdd842f26352b841775e076f7a9
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2023-12-11 10:57:05 +0100

    test: add test seed data data-test.sql

---
+++ spring-backend/src/test/resources/data-test.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user" (username, password, role) VALUES ('User 1', 'TestPassword1234', 'admin');
+INSERT INTO "user" (username, password, role) VALUES ('User 2', 'Pw%FHNWTest', 'user');
+INSERT INTO "user" (username, password, role) VALUES ('User 3', 'S3cUr3Pa$$', 'admin');

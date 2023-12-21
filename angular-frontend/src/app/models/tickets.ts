debb2656c6f557b96b37c6237b1957e02baeba5d
Dobrawa Kiefer <dobrawa.kiefer@students.fhnw.ch>
2023-12-21 23:32:15 +0100

    added ticket.ts

---
+++ angular-frontend/src/app/models/tickets.ts
@@ -0,0 +1,10 @@
+export class Tickets {
+  public ticket_id: string = "";
+  public title: string = "";
+  public description: string = "";
+  public creation_date: string = "";
+  public status: string = "";
+  public user_id: string = "";
+  public user: string = "";
+}
+

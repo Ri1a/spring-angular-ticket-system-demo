ceb47e1730c65cdcfbf24fcfede1efcb54676d3e
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2023-12-23 15:54:53 +0100

    test: add ticket unit test

---
+++ spring-backend/src/test/java/ch/fhnw/webec/exercise/model/TicketUnitTest.java
@@ -0,0 +1,62 @@
+package ch.fhnw.webec.exercise.model;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+class TicketUnitTest {
+
+    private Validator createValidator() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.afterPropertiesSet();
+        return localValidatorFactoryBean;
+    }
+
+    private <T> List<ConstraintViolation<T>> getConstraintViolationsByPropertyPath(Set<ConstraintViolation<T>> constraintViolations, String propertyPath) {
+        return constraintViolations.stream().filter(violation -> violation.getPropertyPath().toString().equals(propertyPath)).toList();
+    }
+
+    @Test
+    void testTitleValidation() {
+        Validator validator = createValidator();
+        Ticket ticket = new Ticket();
+
+        Set<ConstraintViolation<Ticket>> constraintViolations = validator.validate(ticket);
+        List<ConstraintViolation<Ticket>> titleViolations = getConstraintViolationsByPropertyPath(constraintViolations, "title");
+
+        assertEquals("Must not be null", 1, titleViolations.size());
+    }
+
+    @Test
+    void testDescriptionValidation() {
+        Validator validator = createValidator();
+        Ticket ticket = new Ticket();
+
+        Set<ConstraintViolation<Ticket>> constraintViolations = validator.validate(ticket);
+        List<ConstraintViolation<Ticket>> descriptionViolations = getConstraintViolationsByPropertyPath(constraintViolations, "description");
+
+        assertEquals("Must not be null", 1, descriptionViolations.size());
+    }
+
+    @Test
+    void testCommentsRelation() {
+        Ticket ticket = new Ticket();
+        Comment comment1 = new Comment();
+        Comment comment2 = new Comment();
+
+        ticket.setComments(List.of(comment1, comment2));
+
+        assertEquals("", 2, ticket.getComments().size());
+        assertTrue(ticket.getComments().contains(comment1));
+        assertTrue(ticket.getComments().contains(comment2));
+    }
+}

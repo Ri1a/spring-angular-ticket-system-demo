d139b5f937d934143975e504e2e4189199d95f6c
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-03 00:47:33 +0100

    feat: add UserService.java

---
+++ spring-backend/src/main/java/ch/fhnw/webec/exercise/service/UserService.java
@@ -0,0 +1,19 @@
+package ch.fhnw.webec.exercise.service;
+
+import ch.fhnw.webec.exercise.model.User;
+import ch.fhnw.webec.exercise.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}

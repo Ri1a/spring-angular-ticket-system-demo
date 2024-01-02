82cf027767dec6743bc2ac6c624bc2322581cb2e
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-03 00:46:09 +0100

    feat: add auth controller

---
+++ spring-backend/src/main/java/ch/fhnw/webec/exercise/controller/AuthController.java
@@ -0,0 +1,32 @@
+package ch.fhnw.webec.exercise.controller;
+
+import ch.fhnw.webec.exercise.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final JwtService jwtService;
+
+
+    @Autowired
+    public AuthController(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/token")
+    public String generateToken(@RequestBody Map<String, String> credentials) {
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+
+        final String jwt = jwtService.generateToken(username);
+        return "Bearer " + jwt;
+    }
+}

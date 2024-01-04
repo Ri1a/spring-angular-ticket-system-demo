3b86e9c6f89d6a756f66cb5fc2384973da558989
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-05 00:28:27 +0100

    fix: cross origin error for LoginController.java

--- spring-backend/src/main/java/ch/fhnw/webec/exercise/controller/LoginController.java
+++ spring-backend/src/main/java/ch/fhnw/webec/exercise/controller/LoginController.java
@@ -6,11 +6,13 @@ import ch.fhnw.webec.exercise.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class LoginController {
     private final UserService userService;
     private final JwtService jwtService;

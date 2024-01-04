29dc8bec478db3b4bc3ce1742b03cca0a734993d
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-04 09:43:42 +0100

    chore: add data-test.sql

--- spring-backend/src/test/resources/data-test.sql
+++ spring-backend/src/test/resources/data-test.sql
@@ -1,5 +1,3 @@
-INSERT INTO "user" (id, username, password, role) VALUES ('1', 'User 1', 'TestPassword1234', 'admin');
-INSERT INTO "user" (id, username, password, role) VALUES ('2', 'User 2', 'Pw%FHNWTest', 'user');
-INSERT INTO "user" (id, username, password, role) VALUES ('3', 'User 3', 'S3cUr3Pa$$', 'admin');
-
-INSERT INTO "TICKETS" (title, description, creationDate) VALUES ('Ticket 1', 'Ticket 1 description', '19.12.2023');
+INSERT INTO "user" (id, username, password) VALUES ('1', 'User 1', 'TestPassword1234');
+INSERT INTO "user" (id, username, password) VALUES ('2', 'User 2', 'Pw%FHNWTest');
+INSERT INTO "user" (id, username, password) VALUES ('3', 'User 3', 'S3cUr3Pa$$');

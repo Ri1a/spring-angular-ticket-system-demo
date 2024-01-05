51273721aebbd21e190c855079e503efb4cb0c45
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-05 16:04:00 +0100

    refactor: remove request headers user.service.ts

--- angular-frontend/src/app/services/user.service.ts
+++ angular-frontend/src/app/services/user.service.ts
@@ -1,39 +1,42 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {User} from "../models/user";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
-
-  private REST_API_SERVER = "http://localhost:8080/api"
-  constructor(private httpClient:HttpClient) { }
+  private REST_API_SERVER = 'http://localhost:8080/api';
+  constructor(private httpClient: HttpClient) {}
 
   public getAllUser(): Observable<Array<User>> {
-    return this.httpClient.get<Array<User>>(this.REST_API_SERVER + "/user/getAllUser", {headers: UserService.getHeaders()})
+    return this.httpClient.get<Array<User>>(
+      this.REST_API_SERVER + '/user/getAllUser',
+      {}
+    );
   }
 
   public updateUser(user: User): Observable<User> {
-    return this.httpClient.post<User>(this.REST_API_SERVER + "/updateUser",user,{headers: UserService.getHeaders()});
+    return this.httpClient.post<User>(
+      this.REST_API_SERVER + '/updateUser',
+      user,
+      {}
+    );
   }
 
   public saveUser(user: User): Observable<User> {
-    return this.httpClient.post<User>(this.REST_API_SERVER + "/saveUser",user,{headers: UserService.getHeaders()} );
+    return this.httpClient.post<User>(
+      this.REST_API_SERVER + '/saveUser',
+      user,
+      {}
+    );
   }
 
   public deleteUser(userID: string): Observable<User> {
-    return this.httpClient.post<User>(this.REST_API_SERVER + "/deleteUser/" + userID,{headers: UserService.getHeaders()});
-  }
-
-  public static getHeaders(contentType: string = 'application/json'): HttpHeaders {
-    let headers = new HttpHeaders().set('Content-Type', contentType);
-    headers = headers.append('Cache-Control', 'no-cache');
-    headers = headers.append('Pragma', 'no-cache');
-    headers = headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
-    headers =  headers.append('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-    return headers;
+    return this.httpClient.post<User>(
+      this.REST_API_SERVER + '/deleteUser/' + userID,
+      {}
+    );
   }
 }

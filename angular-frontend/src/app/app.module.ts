e2393c574ae48757bee1b415cfae1277fa8a301f
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-06 17:31:33 +0100

    feat: add input validation

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,36 +1,39 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {NewTicketComponent} from './component/ticket/overview/new-ticket/new-ticket.component';
+import { AppComponent } from './app.component';
+import { NewTicketComponent } from './component/ticket/overview/new-ticket/new-ticket.component';
 
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {OverviewComponent} from './component/ticket/overview/overview/overview.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { OverviewComponent } from './component/ticket/overview/overview/overview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatSliderModule} from '@angular/material/slider';
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatCardModule} from "@angular/material/card";
-import {DragDropModule} from "@angular/cdk/drag-drop";
-import {MatDialogModule, MatDialogRef} from "@angular/material/dialog";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatDatepicker, MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule, MatOptionModule} from "@angular/material/core";
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
-import {MatButtonModule} from "@angular/material/button";
-import {FormsModule} from "@angular/forms";
+import { MatSliderModule } from '@angular/material/slider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MatDatepicker,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LogInComponent } from './component/log-in/log-in.component';
-import {RouterModule} from "@angular/router";
-import {AppRoutingModule} from "./app-routing.module";
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { SettingsComponent } from './component/settings/settings.component';
 import { NewUserComponent } from './component/new-user/new-user.component';
-import {BasicAuthInterceptorService} from "./services/basic-auth-interceptor.service";
+import { BasicAuthInterceptorService } from './services/basic-auth-interceptor.service';
 import { AboutComponent } from './component/about/about.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -62,24 +65,27 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatOptionModule,
     MatButtonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([]),
     AppRoutingModule,
-    MatTabsModule
+    MatTabsModule,
   ],
-  providers: [{
-    provide: MatDialogRef,
-    useValue: {}
-  },{
-    provide: MatDatepicker,
-    useValue: {}
-  },
+  providers: [
+    {
+      provide: MatDialogRef,
+      useValue: {},
+    },
+    {
+      provide: MatDatepicker,
+      useValue: {},
+    },
     {
       // token interceptor
       provide: HTTP_INTERCEPTORS,
-      useClass:BasicAuthInterceptorService,
-      multi: true
+      useClass: BasicAuthInterceptorService,
+      multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

819856938d110d8c508d8c31f2073282b6cd582b
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-06 20:41:23 +0100

    refactor: fix settings page

--- angular-frontend/src/app/component/settings/settings.component.ts
+++ angular-frontend/src/app/component/settings/settings.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from "../../models/user";
-import {MatDialog} from "@angular/material/dialog";
-import {UserService} from "../../services/user.service";
-import {NewUserComponent} from "../new-user/new-user.component";
+import { User } from '../../models/user';
+import { MatDialog } from '@angular/material/dialog';
+import { UserService } from '../../services/user.service';
+import { NewUserComponent } from '../new-user/new-user.component';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrl: './settings.component.css',
 })
 export class SettingsComponent implements OnInit {
-
   userRoleAdmin: User[] = [];
   userRoleNormal: User[] = [];
 
-  constructor(private userService: UserService,
-              public dialog: MatDialog) {
-  }
+  constructor(private userService: UserService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.loadAllUser();
   }
 
   loadAllUser(): void {
-    this.userService.getAllUser().subscribe(result => {
-      this.userRoleAdmin = result.filter(x => x.role === "Admin")
-      this.userRoleNormal = result.filter(x => x.role === "Normal")
+    this.userService.getAllUser().subscribe((result) => {
+      this.userRoleAdmin = result.filter((user) =>
+        user.authorities.some((auth) => auth.authority === 'ROLE_ADMIN')
+      );
+      this.userRoleNormal = result.filter((user) =>
+        user.authorities.some((auth) => auth.authority === 'ROLE_USER')
+      );
     });
+  }
+
+  getRole(user: User): string {
+    return user.authorities.some((auth) => auth.authority === 'ROLE_ADMIN')
+      ? 'Admin'
+      : 'User';
   }
 
   openDialogNewUser(): void {
@@ -37,15 +44,13 @@ export class SettingsComponent implements OnInit {
       data: new User(),
     });
 
-    dialogRef.afterClosed().subscribe(
-      () =>   this.loadAllUser()
-    );
+    dialogRef.afterClosed().subscribe(() => this.loadAllUser());
   }
 
   deleteUser(user_id: string): void {
-    this.userService.deleteUser(user_id).subscribe(result => {
+    this.userService.deleteUser(user_id).subscribe((result) => {
       this.loadAllUser();
-    })
+    });
   }
 
   openDialogEdit(user: User): void {
@@ -53,10 +58,8 @@ export class SettingsComponent implements OnInit {
       width: '500px',
       height: '450px',
       panelClass: 'my-dialog',
-      data: user
+      data: user,
     });
-    dialogRef.afterClosed().subscribe(
-      () =>   this.loadAllUser()
-    );
+    dialogRef.afterClosed().subscribe(() => this.loadAllUser());
   }
 }

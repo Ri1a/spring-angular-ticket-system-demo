06477e018e03b0fc4c9d1462044d83ef2cbac19b
Dobrawa Kiefer <dobrawa.kiefer@students.fhnw.ch>
2024-01-06 21:29:00 +0100

    add: title attribute to Project

--- spring-backend/src/main/java/ch/fhnw/webec/exercise/model/Project.java
+++ spring-backend/src/main/java/ch/fhnw/webec/exercise/model/Project.java
@@ -16,6 +16,16 @@ public class Project {
     @ManyToOne
     private User user;
 
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getId() {
         return id;
     }
@@ -23,7 +33,6 @@ public class Project {
     public void setId(String id) {
         this.id = id;
     }
-
 
     public List<Ticket> getTickets() {
         return tickets;

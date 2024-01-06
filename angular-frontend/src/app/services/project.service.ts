4598ef2bcfc323173b5199d0385eb2bc5daab4a1
Dobrawa Kiefer <dobrawa.kiefer@students.fhnw.ch>
2024-01-06 21:31:42 +0100

    add: project service

---
+++ angular-frontend/src/app/services/project.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tickets } from '../models/tickets';
+import {tick} from "@angular/core/testing";
+import {Project} from "../models/project";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  private REST_API_SERVER = 'http://localhost:8080/api/projects';
+  constructor(private httpClient: HttpClient) {}
+
+  public getAllProjects(): Observable<Array<Project>> {
+    return this.httpClient.get<Array<Project>>(this.REST_API_SERVER, {});
+  }
+
+  public addProject(project: Project): Observable<Project> {
+    return this.httpClient.post<Project>(
+      this.REST_API_SERVER + '/add',
+      project,
+      {}
+    );
+  }
+
+  public updateProject(project: Project): Observable<Project> {
+    return this.httpClient.post<Project>(
+      this.REST_API_SERVER + '/' + project.id + '/edit',
+      project,
+      {}
+    );
+  }
+
+  public deleteProject(projectId: string): Observable<Project> {
+    return this.httpClient.post<Project>(
+      this.REST_API_SERVER + '/' + projectId + '/delete',
+      {}
+    );
+  }
+}

9f1d5022f5f485d8905744d9ed4ef8acd2deadab
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-06 20:40:16 +0100

    chore: add project data

--- spring-backend/src/main/resources/data.sql
+++ spring-backend/src/main/resources/data.sql
@@ -1,10 +1,8 @@
--- Create authority table if not exists
 CREATE TABLE IF NOT EXISTS "authority" (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL
     );
 
--- Create user_authority table if not exists
 CREATE TABLE IF NOT EXISTS "user_authority" (
     user_id UUID NOT NULL,
     authority_id UUID NOT NULL,
@@ -12,6 +10,13 @@ CREATE TABLE IF NOT EXISTS "user_authority" (
     FOREIGN KEY (user_id) REFERENCES "user"(id),
     FOREIGN KEY (authority_id) REFERENCES "authority"(id)
     );
+
+
+CREATE TABLE IF NOT EXISTS "project" (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
 
 -- Insert data into authority table if not exists
 INSERT INTO authority (id, name)
@@ -40,16 +45,23 @@ SELECT '1867e482-a0ab-11ee-8c90-0242ac120002', 'authority-id-1'
     SELECT 1 FROM user_authority WHERE user_id = '1867e482-a0ab-11ee-8c90-0242ac120002'
 );
 
--- Insert admin user with a check to avoid duplicates
+-- Insert the admin user (if not exists)
 INSERT INTO "user" (id, username, password)
 SELECT '7942010a-3e2f-484e-8b04-41f0adb8d9e6', 'admin', 'admin'
     WHERE NOT EXISTS (
     SELECT 1 FROM "user" WHERE username = 'admin'
 );
 
--- Associate admin user with ROLE_ADMIN authority
+-- Associate the admin user with ROLE_ADMIN authority (if not exists)
 INSERT INTO user_authority (user_id, authority_id)
 SELECT '7942010a-3e2f-484e-8b04-41f0adb8d9e6', 'authority-id-2'
     WHERE NOT EXISTS (
     SELECT 1 FROM user_authority WHERE user_id = '7942010a-3e2f-484e-8b04-41f0adb8d9e6'
+);
+
+-- Insert a new project and associate it with the admin user (if not exists)
+INSERT INTO "project" (id, user_id)
+SELECT 'e5e6da46-1bb6-4f64-b765-0a67aab375d9', '7942010a-3e2f-484e-8b04-41f0adb8d9e6'
+    WHERE NOT EXISTS (
+    SELECT 1 FROM "project" WHERE user_id = '7942010a-3e2f-484e-8b04-41f0adb8d9e6'
 );

47a9f7388f69bd95b87da9faf9c11dbc43789bf9
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-07 15:36:07 +0100

    fix: auth guard service login redirect

--- angular-frontend/src/app/services/auth-guard.service.ts
+++ angular-frontend/src/app/services/auth-guard.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
-import {AuthenticationService} from "./authentication.service";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
 
-  constructor(private router: Router,
-              private authService: AuthenticationService) { }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.authService.isUserLoggedIn())
-      return true;
-
-    this.router.navigate(['login']);
-    return false;
+  canActivate(): boolean {
+    if (!this.authService.isUserLoggedIn()) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
   }
 }

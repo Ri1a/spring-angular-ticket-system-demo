9157d6a5de2e30a08cd53f79f2ac686b46dccecf
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-07 15:10:50 +0100

    feat: add form validation to login

--- angular-frontend/src/app/component/log-in/log-in.component.ts
+++ angular-frontend/src/app/component/log-in/log-in.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from "../../services/user.service";
-import {AuthenticationService} from "../../services/authentication.service";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrl: './log-in.component.css',
 })
 export class LogInComponent implements OnInit {
-
+  loginForm!: FormGroup;
   hide: boolean = true;
 
-  constructor(private userService: UserService, private authService: AuthenticationService) { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
-  logInUser(username: string, password: string ) {
-    this.authService.authenticate(username, password).subscribe(result => console.log(result));
+  logInUser() {
+    if (this.loginForm.valid) {
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+      this.authService
+        .authenticate(username, password)
+        .subscribe((result) => console.log(result));
+    }
   }
 }

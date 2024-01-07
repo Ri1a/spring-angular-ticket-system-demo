9b75a31dadf7c50fb4bc35bed65266a1c7ea2bd7
Riccardo Landolfo (s) <riccardo.landolfo@students.fhnw.ch>
2024-01-07 15:35:03 +0100

    fix: login and redirects

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,24 +1,42 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {OverviewComponent} from "./component/ticket/ticket-overview/overview.component";
-import {LogInComponent} from "./component/log-in/log-in.component";
-import {SettingsComponent} from "./component/settings/settings.component";
-import {AuthGuardService} from "./services/auth-guard.service";
-import {RoleGuardService} from "./services/role-guard.service";
-import {AboutComponent} from "./component/about/about.component";
-import {ProjectComponent} from "./component/project/project-overview/project.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './component/ticket/ticket-overview/overview.component';
+import { LogInComponent } from './component/log-in/log-in.component';
+import { SettingsComponent } from './component/settings/settings.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { RoleGuardService } from './services/role-guard.service';
+import { AboutComponent } from './component/about/about.component';
+import { ProjectComponent } from './component/project/project-overview/project.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'overview/:id', component: OverviewComponent,canActivate:[AuthGuardService]  },
-  { path: 'login', component: LogInComponent  },
-  { path: 'settings', component: SettingsComponent,canActivate:[AuthGuardService, RoleGuardService]},
-  { path: 'about', component: AboutComponent  },
-  { path: 'project', component: ProjectComponent, canActivate:[AuthGuardService]  },
-  { path: '**', redirectTo: '/login' },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'overview/:id',
+    component: OverviewComponent,
+    canActivate: [AuthGuardService],
+  },
+  { path: 'login', component: LogInComponent },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    canActivate: [AuthGuardService, RoleGuardService],
+  },
+  { path: 'about', component: AboutComponent },
+  {
+    path: 'project',
+    component: ProjectComponent,
+    canActivate: [AuthGuardService],
+  },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
